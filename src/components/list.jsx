4fb053a4660556ee448f49e599432316a08337a5
Janhavi Alekar <97527096+JanhaviAlekar@users.noreply.github.com>
2023-03-28 15:52:18 +0530

    Initial commit

---
+++ src/components/list.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.item}</li>
+    </div>
+  );
+}
+export default List;
